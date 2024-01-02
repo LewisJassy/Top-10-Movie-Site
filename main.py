3bb93b4ade38a37a07b63d997c5527e4c76fb433
LewisJassy <jassylewis70@gmail.com>
2024-01-02 17:52:49 +0300

    updated and added a new file to handle (update)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 import requests
 
@@ -32,6 +32,11 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
+# creating a wtform that updated the rating and review
+class MovieForm(FlaskForm):
+    rating = IntegerField('Rating', validators=[DataRequired()])
+    review = StringField("Review", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route("/")
@@ -40,6 +45,23 @@ def home():
     movies_query = db.session.execute(db.select(Movie).order_by(Movie.id)).scalars()
     return render_template("index.html", movies=movies_query)
 
+
+@app.route('/edit', methods=['GET', 'POST'])
+def rate_movie():
+    form = MovieForm()
+    # get the value of the id param from the query string
+    movie_id = request.args.get('id', None)
+    movie = db.get_or_404(Movie, movie_id) if movie_id is not None else None
+    if movie is not None:
+        if form.validate_on_submit():
+            movie.rating = float(form.rating.data)
+            movie.review = form.review.data
+            db.session.commit()
+            return redirect(url_for("home"))
+        return render_template("edit.html", form=form, movie=movie)
+    else:
+        # Handle the case where movie_id is not found
+        return render_template("error.html", error_message="Movie not found")
 
 if __name__ == '__main__':
     app.run(debug=True)
