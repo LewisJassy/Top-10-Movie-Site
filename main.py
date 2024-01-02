dda2c87fe1342f4035bc81a0cb756d650597d44d
LewisJassy <jassylewis70@gmail.com>
2024-01-02 06:46:50 +0300

    create a database

--- main.py
+++ main.py
@@ -11,6 +11,28 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Movies.db"
+
+db.init_app(app)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer)
+    review = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"Title: {self.title}, Year: {self.year}"
+
+with app.app_context():
+    db.create_all()
+
+
 
 @app.route("/")
 def home():
